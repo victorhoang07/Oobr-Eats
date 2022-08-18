7d26f2b30c03418fdfba2aa06e22ac378837d405
Victor Hoang <vikterhoang@gmail.com>
2022-08-18 14:55:36 -0400

    added grid for restaurants to populate

--- frontend/components/restaurant/restaurant_index.jsx
+++ frontend/components/restaurant/restaurant_index.jsx
@@ -1,18 +1,47 @@
 import React from "react";
 
 
-const ResaturantIndex = () => {
+const ResaturantIndex = (props) => {
 
 
     return (
-        <div className="main-filter-container">
-            <p>Sort</p>
-            <p>Price Range</p>
-            <div className="price-range-buttons-container">
-                <button>$</button>
-                <button>$$</button>
-                <button>$$$</button>
-                <button>$$$$</button>
+        <div className="restaurant-index-container">
+            <h2 className="restaurant-index-text">Popular near you </h2>
+            <div className="main-filter-container">
+                <div className="main-filter">
+                    <p>Sort</p>
+                    <p>Price Range</p>
+                    <div className="price-range-buttons-container">
+                        <button>$</button>
+                        <button>$$</button>
+                        <button>$$$</button>
+                        <button>$$$$</button>
+                    </div>
+                </div>
+            </div>
+            <div className="restaurant_index">
+                <div className="restaurants-container">
+                    <div className="restaurant">1</div>
+                    <div className="restaurant">2</div>
+                    <div className="restaurant">3</div>
+                    <div className="restaurant">4</div>
+                    <div className="restaurant">1</div>
+                    <div className="restaurant">2</div>
+                    <div className="restaurant">3</div>
+                    <div className="restaurant">4</div>
+                    <div className="restaurant">1</div>
+                    <div className="restaurant">2</div>
+                    <div className="restaurant">3</div>
+                    <div className="restaurant">4</div>
+                    <div className="restaurant">1</div>
+                    <div className="restaurant">2</div>
+                    <div className="restaurant">3</div>
+                    <div className="restaurant">4</div>
+                    <div className="restaurant">1</div>
+                    <div className="restaurant">2</div>
+                    <div className="restaurant">3</div>
+                    <div className="restaurant">4</div>
+                </div>
             </div>
         </div>
     )
