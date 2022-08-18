45f771255f04d28e77387eda557cbd63e0726039
Victor Hoang <vikterhoang@gmail.com>
2022-08-17 21:30:02 -0400

    got the carousel to work

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -1,13 +1,101 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { logout } from "../../actions/session_actions";
 import { connect } from "react-redux";
 import NavBarContainer from "../nav/navbar";
+import { tns } from "tiny-slider";
+
+const CATEGORIES = {
+        "All": 'https://oobreats.s3.amazonaws.com/deals.png',
+        "Alcohol":'https://oobreats.s3.amazonaws.com/alcohol.png',
+        "Sandwich":'https://oobreats.s3.amazonaws.com/sandwich.png',
+        "Drinks":'https://oobreats.s3.amazonaws.com/coffeeandtea.png',
+        "Comfort Food": 'https://oobreats.s3.amazonaws.com/comfortfood.png',
+        'Italian': 'https://oobreats.s3.amazonaws.com/italian.png',
+        'American': 'https://oobreats.s3.amazonaws.com/american.png',
+        'Pizza': 'https://oobreats.s3.amazonaws.com/pizza.png',
+        'Chinese': 'https://oobreats.s3.amazonaws.com/asian.png',
+        'Japanese': 'https://oobreats.s3.amazonaws.com/sushi.png',
+        'Korean': 'https://oobreats.s3.amazonaws.com/healthy.png'
+    }
+
+const SLIDER = {
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6'
+}
+
 const Main = (props) => {
+
+    useEffect(() => {
+        let slider = tns({
+            container: '.my-slider',
+            'slideBy': 3,
+            'speed': 1500,
+            'nav': false,
+            'controlsContainer': '.controller',
+            "items": 3,
+            "gutter" : 0
+        })
+    }, [])
+
+    const renderCategories = () => {
+        return (
+            <div className="categories-container">
+                {Object.keys(CATEGORIES).map((category, i) => {
+                    return (
+                        <button key={i} className="category-button">
+                            <img className="category-icon" src={CATEGORIES[category]}/>
+                            <div className="category-text">
+                                {category}
+                            </div>
+                        </button>
+                        
+                    )
+                })}
+            </div>
+        )
+    }
+
+    const renderCarousel = () => {
+       
+        return (
+        <div id="slider">
+            <div className="slider-container">
+                    <div className="controller">
+                        <button className="previous">prev</button>
+                        <button className="next">next</button>
+                    </div>
+                    <div className="my-slider">
+                        {Object.keys(SLIDER).map((slide, i) => {
+                            return (
+                                <div key={i}>
+                                    <div className="slide">
+                                            <div>{SLIDER[slide]}</div>
+                                            <img className="slide-img"src="https://oobreats.s3.amazonaws.com/splash1.jpeg" alt=""/>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                
+            </div>
+        </div>
+        );
+        
+    }
+
+
 
     return (
         <div>
             <NavBarContainer />
-            {/* <button onClick={props.logout}>logout</button> */}
+            {renderCategories()}
+            {renderCarousel()}
+
+
         </div>
 
     )
