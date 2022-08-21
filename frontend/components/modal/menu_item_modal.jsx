e7ae999df8ca428fcdfa0dbf54b628a882416dbb
Victor Hoang <vikterhoang@gmail.com>
2022-08-21 12:56:04 -0400

    rendered increment and decrement button

--- frontend/components/modal/menu_item_modal.jsx
+++ frontend/components/modal/menu_item_modal.jsx
@@ -1,22 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 import { connect } from "react-redux";
 import Loading from "../loading/loading";
-
+import { closeModal } from "../../actions/modal_actions";
 const MenuItemModal = (props) => {
     
     const {restaurants, itemId, restaurantId} = props
     const restaurant = restaurants[parseInt(restaurantId)]
     const itemInfo = restaurant.menu[itemId]
-    
+
+    const [amount, setAmount] = useState(1)
+    const [total, setTotal] = useState(itemInfo.price.toFixed(2))
+
+    const handleIncrement = () => {
+        setAmount(amount + 1)
+        setTotal((itemInfo.price.toFixed(2) * (amount + 1)).toFixed(2) )
+    }
+
+    const handleDecrement = () => {
+        if (amount > 1) {
+            setAmount(amount - 1)
+            setTotal((itemInfo.price * (amount - 1)).toFixed(2))
+        }
+    }
+
     if (!restaurant) {
         return (<Loading />)
     } else {
         return(
             <div className="item-modal-container">
-                <img className="item-modal-img" src={itemInfo.img_url} alt="" />
+                <button onClick={() => props.closeModal()} className="exit-item-modal">&#x2716;</button>
+                <img className="item-modal-img" src='https://oobreats.s3.amazonaws.com/restaurantphotos/popeyes.jpeg' alt="" />
                 <div className="item-modal-name">{itemInfo.name}</div>
-                <div className="item-modal-price">{itemInfo.price}</div>
+                <div className="item-modal-price">${itemInfo.price}</div>
                 <div className="item-modal-descript">{itemInfo.description}</div>
+
+                <div className="item-button-container">
+                    <div className="item-modal-buttons">
+                        <button className="item-buttons" onClick={() => handleDecrement()}> - </button>
+                        <span>{amount}</span>
+                        <button className="item-buttons" onClick={() => handleIncrement()}> + </button>
+                    </div>
+                    <button className="add-item-button">Add {amount} to order
+                    <span className="dot"> • </span> 
+                    ${total}
+                    </button>
+                </div>
             </div>
         )
     }
@@ -26,4 +54,8 @@ const mSTP = (state, ownProps) => ({
     restaurants: state.entities.restaurants
 })
 
-export default connect(mSTP,null)(MenuItemModal)
+const mDTP = dispatch => ({
+    closeModal: () => dispatch(closeModal())
+})
+
+export default connect(mSTP,mDTP)(MenuItemModal)
